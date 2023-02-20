a60a72d4a55bdc5c2a9c49a2c774bb79df1cbbad
Milena Teixeira <milenateixeira648@gmail.com>
2023-02-20 12:05:38 -0300

    add: override

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,13 @@
+public class ContaCorrente extends Conta {
+	
+	public ContaCorrente(int agencia, int conta) {
+		super(agencia, conta);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		double valorSaque = valor + .2;
+		return super.saca(valorSaque);
+	}
+	
+}
