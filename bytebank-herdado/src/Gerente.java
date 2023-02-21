b09508577a09ef7953812435f0ee37205beebbd5
Milena Teixeira <milenateixeira648@gmail.com>
2023-02-21 10:16:37 -0300

    add: interface com composição

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -6,24 +6,23 @@
 
 public class Gerente extends Funcionario implements Autenticavel {
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
 	
-	public boolean autentica(int senha){
-		if (this.senha == senha){
-			return true;
-		}
-
-		return false;
+	public Gerente(){
+		this.autenticador = new AutenticacaoUtil();
 	}
 
-	public int getSenha() {
-		return senha;
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
 	}
 
+	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
-	
+
+	@Override
 	public double getBonificacao(){
 		System.out.println("Bonificação GERENTE.");
 		return super.getSalario();
