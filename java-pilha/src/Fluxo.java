cc27f9fee237e71d7f61cd50f243afc036ca94ff
Milena Teixeira <milenateixeira648@gmail.com>
2023-02-22 13:07:03 -0300

    fix: nullpointerexception

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -9,6 +9,9 @@ public class Fluxo {
 		} catch(ArithmeticException arithmeticException){			
 			String msg = arithmeticException.getMessage();
 			System.out.println("Erro de aritmética: " + msg);
+		} catch(NullPointerException nullPointerException){			
+			String msg = nullPointerException.getMessage();
+			System.out.println("Erro de ponteiro nulo: " + msg);
 		}
 
 		System.out.println("Fim do main");
