541a3a8959e3b1bc5897f99f6b1a3ced2bb8ccf2
Milena Teixeira <milenateixeira648@gmail.com>
2023-02-25 13:41:43 -0300

    err: stackoverflow

---
+++ java-pilha/src/FluxoError.java
@@ -0,0 +1,28 @@
+
+public class FluxoError {
+	
+	public static void main(String[] args){
+		System.out.println("Início do main");
+		
+		try {
+			metodo1();
+		} catch(ArithmeticException | NullPointerException | MinhaException exception ){			
+			String msg = exception.getMessage();
+			System.out.println("Erro: " + msg);
+			exception.printStackTrace();
+		}
+
+		System.out.println("Fim do main");
+	}
+	
+	private static void metodo1(){
+		System.out.println("Início do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+	
+	private static void metodo2(){
+		System.out.println("Chamando o método 2.");
+		metodo2();
+	}
+}
